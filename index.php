51380157f9a3dedff1bbf33bf0d0684f3d50944b
xonatis-academy <formation@xonatis.com>
2020-11-16 16:34:24 +0100

    Adding dog

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Chien {
+
+    public int $age;
+    public string $groupeSanguin;
+    public int $taille;
+    public int $poids;
+
+    public function mange() {
+
+    }
+
+    public function dort() {
+
+    }
+
+    public function aboie() {
+
+    }
+}
+
+$rex = new Chien();
+var_dump($rex);
+
+?>
